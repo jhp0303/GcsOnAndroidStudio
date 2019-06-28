e5b87d7af5dd85ee4e800ed313903072dcfd9265
jhp0303 <yingouyingou@gmail.com>
2019-06-28 17:12:46 +0900

    맵 타입을 Hybrid로 변경, MainActivity의 불필요한 import 삭제

--- app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
+++ app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
@@ -3,14 +3,6 @@ package com.jangin.gcsonandroidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.CameraPosition;
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.NaverMapOptions;
 
 public class MainActivity extends AppCompatActivity {
 
