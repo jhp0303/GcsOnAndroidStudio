ccb3571bf82e7f3fc6ec658006a85f414501e2de
jhp0303 <yingouyingou@gmail.com>
2019-06-28 17:04:29 +0900

    네이버 지도에 군산대학교 줌인

--- app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
+++ app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
@@ -3,12 +3,23 @@ package com.jangin.gcsonandroidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
