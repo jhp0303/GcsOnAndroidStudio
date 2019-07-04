0331d7eca61413c6395d7fc0a8d943b12daed628
jhp0303 <yingouyingou@gmail.com>
2019-07-04 17:25:04 +0900

    불필요한 import 삭제 및 ArrayList활용해서 코드 줄임

--- app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
+++ app/src/main/java/com/jangin/gcsonandroidstudio/MainActivity.java
@@ -2,11 +2,9 @@ package com.jangin.gcsonandroidstudio;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
-import android.graphics.Camera;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -16,21 +14,17 @@ import android.widget.Toast;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.geometry.LatLngBounds;
 import com.naver.maps.map.CameraAnimation;
-import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.InfoWindow;
-import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.PolylineOverlay;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends FragmentActivity
         implements OnMapReadyCallback {
@@ -64,123 +58,81 @@ public class MainActivity extends FragmentActivity
     public void onMapReady(@NonNull final NaverMap naverMap) {
         naverMap.setMapType(NaverMap.MapType.Terrain);
 
-        CameraUpdate cameraUpdate = CameraUpdate.scrollAndZoomTo(new LatLng(35.945282, 126.68215299999997), 15)
+        ArrayList<LatLng> markersLatLng = new ArrayList<>();
+        ArrayList<Marker> markersArray = new ArrayList<>();
+        ArrayList<InfoWindow> infoWindowsArray = new ArrayList<>();
+
+        markersLatLng.add(new LatLng(35.945282, 126.68215299999997));    //KSUmarker
+        markersLatLng.add(new LatLng(35.9675829, 126.7368305));          //KSCHmarker
+        markersLatLng.add(new LatLng(35.9703446, 126.95475629999999));   //WKUmarker
+        markersLatLng.add(new LatLng(35.8441821,127.12927769999999));    //JBNUmarker
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollAndZoomTo(markersLatLng.get(0), 15)
                 .animate(CameraAnimation.Fly,5000);
         naverMap.moveCamera(cameraUpdate);
 
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setLocationButtonEnabled(true);
         final LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(new LatLng(35.945282, 126.68215299999997))
-                .include(new LatLng(35.9675829, 126.7368305))
-                .include(new LatLng(35.9703446,126.95475629999999))
-                .include(new LatLng(35.8441821,127.12927769999999))
+                .include(markersLatLng.get(0))
+                .include(markersLatLng.get(1))
+                .include(markersLatLng.get(2))
+                .include(markersLatLng.get(3))
                 .build();
         CameraUpdate cameraCenter = CameraUpdate.fitBounds(bounds, 100);
         naverMap.moveCamera(cameraCenter);
 
-        final Marker KSUmarker = new Marker();
-        KSUmarker.setPosition(new LatLng(35.945282, 126.68215299999997));
-        KSUmarker.setMap(naverMap);
-        KSUmarker.setWidth(Marker.SIZE_AUTO);
-        KSUmarker.setHeight(Marker.SIZE_AUTO);
-        final InfoWindow KSUinfoWindow = new InfoWindow();
-        KSUinfoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산대학교";
-            }
-        });
-        KSUinfoWindow.open(KSUmarker);
+        for (int i=0; i < markersLatLng.size(); i++) {
+            Marker marker = new Marker();
+            marker.setPosition(markersLatLng.get(i));
+            marker.setMap(naverMap);
+            marker.setWidth(Marker.SIZE_AUTO);
+            marker.setHeight(Marker.SIZE_AUTO);
+            markersArray.add(marker);
 
-        final Marker KSCHmarker = new Marker();
-        KSCHmarker.setPosition(new LatLng(35.9675829, 126.7368305));
-        KSCHmarker.setMap(naverMap);
-        KSCHmarker.setWidth(Marker.SIZE_AUTO);
-        KSCHmarker.setHeight(Marker.SIZE_AUTO);
-        final InfoWindow KSCHinfoWindow = new InfoWindow();
-        KSCHinfoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산시청";
-            }
-        });
-        KSCHinfoWindow.open(KSCHmarker);
+            InfoWindow infoWindow = new InfoWindow();
+            infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+                @NonNull
+                @Override
+                public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                    return "정보창";
+                }
+            });
+            infoWindow.open(marker);
+            infoWindowsArray.add(infoWindow);
+        }
 
-        final Marker WKUmarker = new Marker();
-        WKUmarker.setPosition(new LatLng(35.9703446,126.95475629999999));
-        WKUmarker.setMap(naverMap);
-        WKUmarker.setWidth(Marker.SIZE_AUTO);
-        WKUmarker.setHeight(Marker.SIZE_AUTO);
-        final InfoWindow WKUinfoWindow = new InfoWindow();
-        WKUinfoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "원광대학교";
-            }
-        });
-        WKUinfoWindow.open(WKUmarker);
-
-        final Marker JBNUmarker = new Marker();
-        JBNUmarker.setPosition(new LatLng(35.8441821,127.12927769999999));
-        JBNUmarker.setMap(naverMap);
-        JBNUmarker.setWidth(Marker.SIZE_AUTO);
-        JBNUmarker.setHeight(Marker.SIZE_AUTO);
-        final InfoWindow JBNUinfoWindow = new InfoWindow();
-        JBNUinfoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "전북대학교";
-            }
-        });
-        JBNUinfoWindow.open(JBNUmarker);
-
-        final PolylineOverlay polyline = new PolylineOverlay();
+        PolylineOverlay polyline = new PolylineOverlay();
         polyline.setCoords(Arrays.asList(
-                new LatLng(35.945282, 126.68215299999997),
-                new LatLng(35.9675829, 126.7368305),
-                new LatLng(35.9703446,126.95475629999999),
-                new LatLng(35.8441821,127.12927769999999)
+                markersLatLng.get(0),
+                markersLatLng.get(1),
+                markersLatLng.get(2),
+                markersLatLng.get(3)
         ));
         polyline.setMap(naverMap);
         polyline.setColor(Color.CYAN);
 
-        LatLng marker1 = new LatLng(35.945282, 126.68215299999997);
-        LatLng marker2 = new LatLng(35.9675829, 126.7368305);
-        LatLng marker3 = new LatLng(35.9703446, 126.95475629999999);
-        LatLng marker4 = new LatLng(35.8441821,127.12927769999999);
-        final List markers = new ArrayList();
-        markers.add(marker1);
-        markers.add(marker2);
-        markers.add(marker3);
-        markers.add(marker4);
-
         final View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (KSUmarker.getMap() == null) {
+                if (markersArray.get(0).getMap() == null) {
                     // 현재 마커가 지도에 없을 경우 마커를 찍음
-                    KSUmarker.setMap(naverMap);
-                    KSUinfoWindow.open(KSUmarker);
-                    KSCHmarker.setMap(naverMap);
-                    KSCHinfoWindow.open(KSCHmarker);
-                    WKUmarker.setMap(naverMap);
-                    WKUinfoWindow.open(WKUmarker);
-                    JBNUmarker.setMap(naverMap);
-                    JBNUinfoWindow.open(JBNUmarker);
+                    for (int i = 0; i < markersArray.size(); i++) {
+                        markersArray.get(i).setMap(naverMap);
+                        infoWindowsArray.get(i).open(markersArray.get(i));
+                    }
                     polyline.setMap(naverMap);
-                } else {
-                    // 이미 마커가 지도에 있을 경우 닫음
-                    KSUmarker.setMap(null);
-                    KSCHmarker.setMap(null);
-                    WKUmarker.setMap(null);
-                    JBNUmarker.setMap(null);
-                    polyline.setMap(null);
 
+                } else {
+                    // 이미 마커가 지도에 있을 경우 마커를 닫음
+
+                    //for (int i = 0; i < markersArray.size(); i++) {
+                    //    markersArray.get(i).setMap(null);
+                    //}
+                    for (Marker i : markersArray) { //위의 for문과 동일
+                        i.setMap(null);
+                    }
+                    polyline.setMap(null);
                 }
             }
         };
